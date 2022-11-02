4676afce35072edf7fd48f2f09e0d77ac61a5bd4
Neuf eGO <patronus1994@gmail.com>
2022-11-02 17:30:59 +0500

    Добавьте файлы проекта.

---
+++ testBot/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+string botToken = "5641237243:AAH_UA-o4tC_hGqVlDKMbPtYNHvVpP6tCVc";
+var bot = new TelegramBotClient(botToken);
+var receiverOptions = new ReceiverOptions
+{
+    Offset = 0
+};
+using var cts = new CancellationTokenSource();
+bot.StartReceiving(
+    Update, 
+    Error, 
+    receiverOptions,
+    cts.Token
+    );
+Console.WriteLine("Бот вышел в онлайн");
+Console.ReadLine();
+
+
+async static Task Update(ITelegramBotClient bot, Update update, CancellationToken token)
+{
+    if (update.Message is not null)
+    {
+        var message = update.Message;
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(message));
+
+        if (message.Text is not null)
+        {
+            if (message.Text.ToLower().Contains("здорова"))
+            {
+                await bot.SendTextMessageAsync(message.Chat.Id, "Здоровее видали");
+            }
+        }
+        if (message.Photo is not null)
+        {
+            await bot.SendTextMessageAsync(message.Chat.Id, "Крутое фото");
+        }
+    }
+}
+
+static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+{
+    //throw new NotImplementedException();
+    throw new NoNullAllowedException();
+}
